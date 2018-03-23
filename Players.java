f52ffd13acf289303575f3be80e19c438d20356e
Dimitar Mihaylov SE2016 <dimitar.mihaylov.2016@uni.strath.ac.uk>
2018-03-23 04:03:10 +0000

    - added a few methods , no functionality needs work

---
+++ Players.java
@@ -0,0 +1,69 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Players
+{
+
+
+	private ArrayList<Player> players;
+	private int playersCount;
+	
+	
+	
+	public Players()
+	{
+		players = new ArrayList<Player>();
+		playersCount = 0;
+		
+		
+	}
+	public ArrayList<Player> getPlayers() {
+		return players;
+	}
+	public void addPlayer(Player newPlayer)
+	{
+		players.add(newPlayer);
+		playersCount++;
+		
+	}
+	public void removePlayer(String name)
+	{
+		for(int i = 0; i< players.size(); i++)
+		{
+			if(players.get(i).equals(name))
+			{
+				players.remove(i);
+			}
+		}
+	}
+	public Player findPlayer(String name)
+	{
+		for(Player player : players)
+		{
+			if(player.getName().equals(name))
+			{
+				return player;
+			}
+		}
+		throw new IndexOutOfBoundsException();
+	}
+	public int getPlayerCount()
+	{
+		return  playersCount;
+	}
+	public void getAllPlayersAccuracies() {
+
+	}
+	public void getAllPlayersTimes() {
+
+	}
+	public void getAllPlayersCryptogramsPlayed() {
+
+	}
+	public void getAllPlayersCryptos() {
+
+	}
+}
