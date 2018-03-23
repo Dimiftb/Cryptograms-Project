ef4f65ef68777bd976a36e16f52ec1d805493333
gmarkov <cgb16202@uni.strath.ac.uk>
2018-03-23 10:55:30 +0000

    Fully? working Player and Players with tests

--- Players.java
+++ Players.java
@@ -1,36 +1,104 @@
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.InputMismatchException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 public class Players
 {
-	private ArrayList<Player> players;
+	private List<Player> players;
 	private int playersCount;
 	
 	
 	
 	public Players()
 	{
-		players = new ArrayList<Player>();
-		playersCount = 0;
+		players = new LinkedList<Player>();
+		File file = new File("accounts.txt");
+		String tempName;
+		double tempAccuracy;
+		double tempAvg;
+		int tempCompleted;
+		int tempPlayed;
+		try
+		{
+			file.createNewFile();
+		}
+		catch(IOException e)
+		{}
+		try
+		{
+			Scanner readFile = new Scanner(file);
+			while(readFile.hasNextLine())
+			{
+				
+				tempName = readFile.next();
+				tempAccuracy = readFile.nextDouble();
+				tempAvg = readFile.nextDouble();
+				tempPlayed = readFile.nextInt();
+				tempCompleted = readFile.nextInt();
+				players.add(new Player(tempName, tempAccuracy, tempAvg, tempPlayed, tempCompleted));
+			}
+			readFile.close();
+		}
+		catch(InputMismatchException e)
+		{
+			e.printStackTrace();
+		}
+		catch(FileNotFoundException f)
+		{
+			f.printStackTrace();
+		}
+		catch(NoSuchElementException m)
+		{
+		}
+		
+		playersCount = players.size();
 		
 		
 	}
-	public void addPlayer(Player newPlayer)
+	public void addPlayer(String newName)
 	{
-		players.add(newPlayer);
+		if(newName.contains(" "))
+		{
+			System.out.println("Names cannot contain a space.");
+			return;
+		}
+		for(Player pl : players)
+		{
+			if(pl.getName().equals(newName))
+			{
+				System.out.println("Player already exists.");
+				return;
+			}
+		}
+		
+		players.add(new Player(newName, 0, 0, 0, 0));
+			
 		playersCount++;
+		
+		
 		
 	}
 	public void removePlayer(String name)
 	{
 		for(int i = 0; i< players.size(); i++)
 		{
-			if(players.get(i).equals(name))
+			if(players.get(i).getName().equals(name))
 			{
 				players.remove(i);
+				playersCount--;
+				return;
 			}
 		}
 	}
@@ -48,5 +116,76 @@ public class Players
 	public int getPlayerCount()
 	{
 		return  playersCount;
+	}
+	
+	public void savePlayers()
+	{
+		
+		try
+		{
+			FileWriter fw = new FileWriter("accounts.txt", false);
+			BufferedWriter bw = new BufferedWriter(fw);
+		    PrintWriter out = new PrintWriter(bw);
+		    for(Player pl : players)
+		    {
+			    out.print(pl.getName());
+			    out.print(" ");
+			    out.print(pl.getPlayerAccuracy());
+			    out.print(" ");
+			    out.print(pl.getAvgCompletionTime());
+			    out.print(" ");
+			    out.print(pl.getNrOfPlayedCryptograms());
+			    out.print(" ");
+			    out.println(pl.getNrOfCompletedCryptograms());
+		    }
+		    out.close();
+		    bw.close();
+		    fw.close();
+		    
+		} 
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public void showTop10()
+	{
+		Collections.sort(players, new Comparator<Player>() {
+		    @Override
+		    public int compare(Player p1, Player p2) {
+		    	if(p1.getAvgCompletionTime() - p2.getAvgCompletionTime() > 0)
+				{
+					return 1;
+				}
+				return -1;
+		    }
+		});
+		for(int count = 0;count < 10; count++)
+		{
+			System.out.print(players.get(count).getName());
+			System.out.print(": ");
+			players.get(count).showStats();
+		}
+	}
+	public static void main(String args[])
+	{
+		Players testPl = new Players();
+		testPl.addPlayer("Mark");
+		testPl.addPlayer("John");
+		testPl.addPlayer("Josh");
+		testPl.addPlayer("Paul");
+		testPl.addPlayer("Idk");
+		testPl.addPlayer("Nobody cares");
+		testPl.addPlayer("Again");
+		testPl.addPlayer("1");
+		testPl.addPlayer("2");
+		testPl.addPlayer("3");
+		testPl.addPlayer("4");
+		testPl.addPlayer("5");
+		testPl.addPlayer("6");
+		System.out.println(testPl.getPlayerCount());
+		System.out.println("");
+		testPl.showTop10();
+		testPl.savePlayers();
 	}
 }
--- PlayersTest.java
+++ PlayersTest.java
@@ -1,6 +1,8 @@
 import static org.junit.Assert.*;
 
+import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class PlayersTest
@@ -10,19 +12,20 @@ public class PlayersTest
 	public void setUp()
 	{
 		game =  new Players();
-		game.addPlayer(new Player("Mark"));
-		game.addPlayer(new Player("John"));
-		game.addPlayer(new Player("Josh"));
-		game.addPlayer(new Player("Paul"));
-		game.addPlayer(new Player("Idk"));
-		game.addPlayer(new Player("Nobody cares"));
-		game.addPlayer(new Player("Again"));
-		game.addPlayer(new Player("1"));
-		game.addPlayer(new Player("2"));
-		game.addPlayer(new Player("3"));
-		game.addPlayer(new Player("4"));
-		game.addPlayer(new Player("5"));
-		game.addPlayer(new Player("6"));
+		game.addPlayer("Mark");
+		game.addPlayer("John");
+		game.addPlayer("Josh");
+		game.addPlayer("Paul");
+		game.addPlayer("Idk");
+		game.addPlayer("Nobody_cares");
+		game.addPlayer("Again");
+		game.addPlayer("1");
+		game.addPlayer("2");
+		game.addPlayer("3");
+		game.addPlayer("4");
+		game.addPlayer("5");
+		game.addPlayer("6");
+		game.savePlayers();
 	}
 	@Test
 	public void playersExistTest()
@@ -32,21 +35,21 @@ public class PlayersTest
 	@Test
 	public void addPlayerTest()
 	{
-		game.addPlayer(new Player("Mark"));
+		
+		game.addPlayer("Stan");
 		assertEquals(game.getPlayerCount(), 14);
 	}
 	@Test
 	public void findPlayerTest()
 	{
-		assertEquals(game.getPlayerCount(), 13);
+		assertEquals(game.getPlayerCount(), 14);
 		assertEquals(game.findPlayer("4").getName(), "4");
 	}
 	@Test
 	public void removePlayerTest()
 	{
-		game.removePlayer("Mark");
+		game.removePlayer("1");
 		assertEquals(game.getPlayerCount(), 13);
 	}
-	
 
 }
