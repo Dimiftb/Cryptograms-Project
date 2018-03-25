b56a1cd4411aac5fbcfa8e98267bcff0db6ef871
Dimitar Mihaylov SE2016 <dimitar.mihaylov.2016@uni.strath.ac.uk>
2018-03-25 03:25:23 +0100

    Some more work done. Communication between cryptogram classes still a bit unclear, needs more work

--- LetterCryptogram.java
+++ LetterCryptogram.java
@@ -2,14 +2,31 @@ import java.util.HashMap;
 
 public class LetterCryptogram extends Cryptogram {
 
-	HashMap<Integer, Character> letterMapping;
-	String phrase;
-
-	public LetterCryptogram() {
+	private HashMap<Integer, Character> letterMapping;
+	private HashMap<Character, Integer> keys;
+	private String phrase;
+	private String encryptedPhrase;
+	
+	
+	/*public LetterCryptogram(String sentance, HashMap<Character, Character> letterMappings) {
+		Random randMapping = new Random();
+		int randomNumber;
 		phrase = super.phrase;
 		for (int i = 0; i < phrase.length(); i++) {
-			letterMapping.put(i, phrase.charAt(i));
+			randomNumber = randMapping.nextInt();
+			letterMapping.put(randomNumber, phrase.charAt(i));
+			keys.put(phrase.charAt(i), randomNumber);
 		}
+	}*/
+	public LetterCryptogram(String encryptedPhrase, HashMap<Character, Character> letterMapping)
+	{
+		super(encryptedPhrase, letterMapping);
+	}
+
+
+	public HashMap<Integer, Character> getLetterMapping()
+	{
+		return letterMapping;
 	}
 
 	public char getLetter(int i) {
@@ -20,5 +37,15 @@ public class LetterCryptogram extends Cryptogram {
 		}
 
 	}
-
+	public String getEncryptedPhrase()
+	{
+		StringBuilder encryptedPhraseBuild = new StringBuilder();
+		for(int count = 0; count < phrase.length(); count++)
+		{
+			encryptedPhraseBuild.append(keys.get(phrase.charAt(count)));
+			encryptedPhraseBuild.append(",");
+		}
+		encryptedPhrase = encryptedPhraseBuild.toString();
+		return encryptedPhrase;
+	}
 }
