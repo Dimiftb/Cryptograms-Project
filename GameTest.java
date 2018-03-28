93aac9d602710a01968b35eda7b5d3f8573aa548
U-DS\xrb16171 <xrb16171@CIS-TELFORD-17.cis.strath.ac.uk>
2018-03-28 21:12:02 +0100

    added final test class

--- GameTest.java
+++ GameTest.java
@@ -6,24 +6,55 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class GameTest {
+	private Cryptogram crypt;
+
+
 	@Before
-	public void setUp()
-	{
-		String phrase = "this is a test";
-		HashMap<Integer,Character> testMap = new HashMap<Integer,Character>();
-		HashMap<Integer,Character> testMap2 = new HashMap<Integer,Character>();
-		//Cryptogram test = new NumberCryptogram
+	public void setUp() {
+		String phrase = "test";
+		HashMap<Integer, Character> testMap = new HashMap<Integer, Character>();
+		HashMap<Character, Integer> testMap2 = new HashMap<Character, Integer>();
+		HashMap<Character, String> ptestMap = new HashMap<Character, String>();
+		HashMap<String, Character>  ptestMap2 = new HashMap<String, Character>();
+		testMap.put(1, 't');
+		testMap.put(2, 'e');
+		testMap.put(3, 's');
+		testMap2.put('t', 1);
+		testMap2.put('e', 2);
+		testMap2.put('s', 3);
+		ptestMap.put('t', Integer.toString(1));
+		ptestMap.put('e', Integer.toString(2));
+		ptestMap.put('s', Integer.toString(3));
+		ptestMap2.put(Integer.toString(1), 't');
+		ptestMap2.put(Integer.toString(2), 'e');
+		ptestMap2.put(Integer.toString(3), 's');
+
+		crypt = new NumberCryptogram(phrase, testMap, testMap2, ptestMap, ptestMap2);
+		// Cryptogram test = new NumberCryptogram
 	}
+
 	@Test
-	public void testAllPlayers() {
-		//assertEquals(newGame.getPlayers(), )
+	public void getPhraseTest() {
+		assertEquals(crypt.getPhrase(), "test");
+	}
+
+	@Test
+	public void incompleteCheckTest() {
+		assertEquals(crypt.completeCheck(), false);
 	}
 	
-	public void getPhraseTest(){
-		//assertEquals();
+	@Test
+	public void progressTest(){
+		assertEquals(crypt.getProgress(), "1 2 3 1 ");
+		crypt.updateProgress("t", "1");
+		assertEquals(crypt.getProgress(), "t 2 3 t ");
 	}
+	
 	public void completeCheckTest() {
-		//assertEquals();
+		crypt.updateProgress("t", "1");
+		crypt.updateProgress("e", "2");
+		crypt.updateProgress("s", "3");
+		assertEquals(crypt.completeCheck(), true);
 	}
-	
+
 }
